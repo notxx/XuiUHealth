757e139de59bfdf36c34e457b038ad1b2cac3124
notXX <notxx@office>
2018-03-01 17:09:48 +0800

    初始提交

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     XuiUHealth Systemless     "
   ui_print "*******************************"
 }
 
